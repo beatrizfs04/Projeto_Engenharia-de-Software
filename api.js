b85856dfe73b11d5f57c63caf313ef44c493e580
oSLaYN <rodrigo.6640@gmail.com>
2024-12-05 10:23:28 +0000

    Logout Done

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ api.get('/profile/', async (req, res) => {
     sql.query('SELECT * FROM users WHERE id=?', [userId], async (err, results) => {
         if (err) { res.status(500).json({message: 'An Error Occurred.'}); }
         if (results.length > 0) {
-            return res.status(200).json(JSON.parse(JSON.stringify(results)));
+            return res.status(200).json(JSON.parse(JSON.stringify(results[0])));
         }
         return res.status(200).json([]); 
     });
